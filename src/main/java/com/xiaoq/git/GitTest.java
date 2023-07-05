7054c19082bb3e1101e06742bef4a0d3d9dcfb56
小白来了 <497732641@qq.com>
2023-07-05 13:57:19 +0000

    update src/main/java/com/xiaoq/git/GitTest.java.
    Signed-off-by: 小白来了 <497732641@qq.com>

--- src/main/java/com/xiaoq/git/GitTest.java
+++ src/main/java/com/xiaoq/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master test ...");
         System.out.println("hello hot-fix2 ...");
         System.out.println("hello gitee ...");
+        System.out.println("hello gitee2 ...");
     }
 }
