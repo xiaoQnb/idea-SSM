9b8f96c4477fb574394d295d2c46e729449c8786
Xiaoq <497732641@qq.com>
2023-07-05 21:55:29 +0800

    gitee commit

--- src/main/java/com/xiaoq/git/GitTest.java
+++ src/main/java/com/xiaoq/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello hot-fix1 ...");
         System.out.println("master test ...");
         System.out.println("hello hot-fix2 ...");
+        System.out.println("hello gitee ...");
     }
 }
